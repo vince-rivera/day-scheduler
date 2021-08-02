9cb48b02f1af9608600ba72204fe239349cc640c
Vince Rivera <riverav@me.com>
2021-08-01 20:25:52 -0500

    created a Javascript function for getting the month, date, and day of the week. Added on load function to body of HTML

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 //when page loads
 //get data from localstorage
-//set the inputs text value according to their time slots
-// add an onclick listener to all buttons
+function setData() {
+    getDate();
+    getData();
+    highlightRows();
+}
 
-// function saveTasks() {
-//     var text = document.getElementById(hourName).value;
-//      console.log(text);   
+function getDate() {
+    //java function for dates
+    var weekday = weekdays[d.getDay()];
+    var month = months[d.getMonth()];
+    var day = d.getDate();
+    var dayEnding = "th";
 
+    if (day === 1) dayEnding = "st";
+    if (day === 2) dayEnding = "nd";
+    if (day === 3) dayEnding = "rd";
+
+    document.getElementById('currentDay').innerHTML = `${weekday}, ${month} ${day}${dayEnding}`;
+}
+
+function getData() {
+
+}
+function highlightRows() {
     
-//  };
-//when buttons are clicked prevent default and call handleSaves functions
-
-    //declare button in HTML with set value in accordance with timeslot
-    //get the value from the event target (the button)
-    //declare and input for each time slot with a unique id "timeAm-input"
-    //set data to local storage
-    //get the input value for the time slot associated with the button (using the event.target.value)
+}
+//declare button in HTML with set value in accordance with timeslot
+//get the value from the event target (the button)
+//declare and input for each time slot with a unique id "timeAm-input"
+//set data to local storage
+//get the input value for the time slot associated with the button (using the event.target.value)
 
